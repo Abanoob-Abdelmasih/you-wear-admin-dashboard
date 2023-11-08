9b81fba4ac565f19c7c06e082664668b1f65f55e
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-08 10:50:52 +0300

    Update hero-detail.module.ts

--- src/app/pages/hero-detail/hero-detail.module.ts
+++ src/app/pages/hero-detail/hero-detail.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { HeroDetailRoutingModule } from './hero-detail-routing.module';
 import { HeroDetailComponent } from './hero-detail.component';
+import { NbCardModule } from '@nebular/theme';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+
 
 
 @NgModule({
   declarations: [HeroDetailComponent],
   imports: [
     CommonModule,
-    HeroDetailRoutingModule
+    HeroDetailRoutingModule,
+    NbCardModule,
+    Ng2SmartTableModule
   ]
 })
 export class HeroDetailModule { }
