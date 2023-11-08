156fdebe1dd30b92d07b045c08273cb8cc8258d8
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-08 22:22:59 +0300

    Update pages.module.ts

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,7 +7,7 @@ import { DashboardModule } from './dashboard/dashboard.module';
 import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
-import { HeroDetailModule } from './hero-detail/hero-detail.module';
+import { HeroDetailModule } from './all-sizes/all-sizes.module';
 
 @NgModule({
   imports: [
