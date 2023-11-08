ac3d23bc405e2ff8a0a74cc50d9d24484c5c095a
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-08 23:09:48 +0300

    Update sizes.module.ts

--- src/app/pages/sizes/sizes.module.ts
+++ src/app/pages/sizes/sizes.module.ts
@@ -6,9 +6,10 @@ import { AllSizesComponent } from "./all-sizes/all-sizes.component";
 import { NbCardModule } from "@nebular/theme";
 import { Ng2SmartTableModule } from "ng2-smart-table";
 import { ButtonModule } from "../../Components/button/button.module";
+import { SizeFormComponent } from "./size-form/size-form.component";
 
 @NgModule({
-  declarations: [AllSizesComponent],
+  declarations: [AllSizesComponent, SizeFormComponent],
   imports: [
     CommonModule,
     HeroDetailRoutingModule,
