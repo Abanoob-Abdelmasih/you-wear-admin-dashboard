991199b2c9b24391a71b8e465b9190be7816f4bb
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-09 00:54:40 +0300

    Update button.component.ts

--- src/app/Components/button/button.component.ts
+++ src/app/Components/button/button.component.ts
@@ -1,19 +1,29 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { ViewCell } from "ng2-smart-table";
+import { SizeService } from "../../Services/Size/size.service";
 @Component({
   selector: "ngx-button",
   templateUrl: "./button.component.html",
   styleUrls: ["./button.component.scss"],
 })
 export class ButtonComponent implements ViewCell {
+  constructor(private sizeService: SizeService) {}
   @Input() value: string | number;
   @Input() rowData: any;
   @Input() type: any;
   @Input() action: any;
   @Output() saves: EventEmitter<any> = new EventEmitter();
-  constructor() {}
 
   deleteRow(id: number) {
-    alert(id);
+    this.sizeService.deleteSize(id).subscribe({
+      next: (response) => {
+        if (response.status === 200) {
+          if (response.body.status === 200) {
+            // this.router.navigate(["/sizes"]);
+          }
+        }
+      },
+      error: (err) => {},
+    });
   }
 }
