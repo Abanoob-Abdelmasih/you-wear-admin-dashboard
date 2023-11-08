64f583f6ebaf0a8dc671427d353dc66f08289227
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-08 23:09:43 +0300

    Create size-form.component.ts

---
+++ src/app/pages/sizes/size-form/size-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-size-form',
+  templateUrl: './size-form.component.html',
+  styleUrls: ['./size-form.component.scss']
+})
+export class SizeFormComponent {
+
+}
