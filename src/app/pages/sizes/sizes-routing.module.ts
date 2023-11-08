f72aac1e4b2cb7118b2798ae0bfb44c48dd364df
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-08 23:00:23 +0300

    Create sizes-routing.module.ts

---
+++ src/app/pages/sizes/sizes-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AllSizesComponent } from './all-sizes/all-sizes.component';
+
+const routes: Routes = [
+  { path: '', component:  AllSizesComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HeroDetailRoutingModule { }
