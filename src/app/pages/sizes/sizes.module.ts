e1107b4d5ad77fb9c42b9958f003e49940f85e76
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-08 23:40:33 +0300

    Update sizes.module.ts

--- src/app/pages/sizes/sizes.module.ts
+++ src/app/pages/sizes/sizes.module.ts
@@ -1,21 +1,42 @@
 import { NgModule } from "@angular/core";
+import {
+  NbActionsModule,
+  NbButtonModule,
+  NbCardModule,
+  NbCheckboxModule,
+  NbDatepickerModule,
+  NbIconModule,
+  NbInputModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbUserModule,
+} from "@nebular/theme";
 import { CommonModule } from "@angular/common";
 
-import { HeroDetailRoutingModule } from "./sizes-routing.module";
+import { SizeRoutingModule } from "./sizes-routing.module";
 import { AllSizesComponent } from "./all-sizes/all-sizes.component";
-import { NbCardModule } from "@nebular/theme";
 import { Ng2SmartTableModule } from "ng2-smart-table";
 import { ButtonModule } from "../../Components/button/button.module";
 import { SizeFormComponent } from "./size-form/size-form.component";
+import { ThemeModule } from "../../@theme/theme.module";
 
 @NgModule({
   declarations: [AllSizesComponent, SizeFormComponent],
   imports: [
     CommonModule,
-    HeroDetailRoutingModule,
-    NbCardModule,
     Ng2SmartTableModule,
-    ButtonModule
+    SizeRoutingModule,
+    ThemeModule,
+    NbInputModule,
+    NbCardModule,
+    NbButtonModule,
+    NbActionsModule,
+    NbUserModule,
+    NbCheckboxModule,
+    NbRadioModule,
+    NbDatepickerModule,
+    NbSelectModule,
+    NbIconModule,
   ],
 })
 export class HeroDetailModule {}
