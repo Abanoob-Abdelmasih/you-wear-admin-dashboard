21655474fbd1919c83f8884e9bfdb55b15746197
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-08 23:00:30 +0300

    Create sizes.module.ts

---
+++ src/app/pages/sizes/sizes.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { HeroDetailRoutingModule } from "./sizes-routing.module";
+import { AllSizesComponent } from "./all-sizes/all-sizes.component";
+import { NbCardModule } from "@nebular/theme";
+import { Ng2SmartTableModule } from "ng2-smart-table";
+import { ButtonModule } from "../../Components/button/button.module";
+
+@NgModule({
+  declarations: [AllSizesComponent],
+  imports: [
+    CommonModule,
+    HeroDetailRoutingModule,
+    NbCardModule,
+    Ng2SmartTableModule,
+    ButtonModule
+  ],
+})
+export class HeroDetailModule {}
