dae78ecfbc4820f72084b9fc1023c3678bc1e2fc
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-08 10:50:55 +0300

    Update hero-detail.component.ts

--- src/app/pages/hero-detail/hero-detail.component.ts
+++ src/app/pages/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { SizeService } from "../../Services/Size/size.service";
+import { LocalDataSource } from "ng2-smart-table";
+import { SmartTableData } from "../../@core/data/smart-table";
+import { ButtonComponent } from "../../Components/button/button.component";
 
 @Component({
   selector: "ngx-hero-detail",
@@ -7,7 +10,63 @@ import { SizeService } from "../../Services/Size/size.service";
   styleUrls: ["./hero-detail.component.scss"],
 })
 export class HeroDetailComponent implements OnInit {
-  constructor(private sizeService: SizeService) {}
+  constructor(
+    private sizeService: SizeService,
+    private service: SmartTableData
+  ) {
+    // const data = this.service.getData();
+    // console.log(data);
+    // this.source.load(data);
+  }
+
+  // source: LocalDataSource = new LocalDataSource();
+
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      id: {
+        title: "ID",
+        type: "number",
+      },
+      name: {
+        title: "Name",
+        type: "string",
+      },
+      abbreviation: {
+        title: "Abbreviation",
+        type: "string",
+      },
+      isActive: {
+        title: "Active",
+        type: "boolean",
+      },
+      edit: {
+        title: "Edit",
+        type: "custom",
+        renderComponent: ButtonComponent,
+      },
+    },
+    actions: {
+      add: false,
+      edit: false,
+      delete: false,
+    },
+  };
+
+  data = [];
 
   ngOnInit(): void {
     const getParams = {};
@@ -16,11 +75,25 @@ export class HeroDetailComponent implements OnInit {
       next: (response) => {
         if (response.status === 200) {
           if (response.body.status === 200) {
-            console.log(response.body.data.all_sizes);
+            // console.log(response.body.data.all_sizes);
+            this.data = response.body.data.all_sizes;
           }
         }
       },
       error: (err) => {},
     });
+  }
+
+  // constructor(private service: SmartTableData) {
+  //   const data = this.service.getData();
+  //   this.source.load(data);
+  // }
+
+  onDeleteConfirm(event): void {
+    if (window.confirm("Are you sure you want to delete?")) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
   }
 }
