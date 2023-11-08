9cd7fd053b1f5dee5835b86945d4b495a0800035
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-09 00:29:11 +0300

    Update size-form.component.ts

--- src/app/pages/sizes/size-form/size-form.component.ts
+++ src/app/pages/sizes/size-form/size-form.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { SizeService } from "../../../Services/Size/size.service";
 
 @Component({
-  selector: 'ngx-size-form',
-  templateUrl: './size-form.component.html',
-  styleUrls: ['./size-form.component.scss']
+  selector: "ngx-size-form",
+  templateUrl: "./size-form.component.html",
+  styleUrls: ["./size-form.component.scss"],
 })
-export class SizeFormComponent {
+export class SizeFormComponent implements OnInit {
+  sizeName: string;
+  abbreviation: string;
+  activate: boolean | undefined;
+  constructor(private sizeService: SizeService) {}
 
+  ngOnInit(): void {
+    this.activate = this.activate === undefined ? true : this.activate;
+    console.log("on itit");
+  }
+
+  // toggleActivate(activate: boolean) {
+  //   // this.activate = activate;
+  //   console.log(activate)
+  // }
+
+  addSizeFuntion() {
+    const postParams = {
+      name: this.sizeName,
+      abbreviation: this.abbreviation,
+      isActive: this.activate,
+    };
+
+    console.log(postParams);
+
+    // this.sizeService.addSize({}, postParams).subscribe({
+    //   next: (response) => {
+    //     if (response.status === 200) {
+    //       if (response.body.status === 200) {
+    //       }
+    //     }
+    //   },
+    //   error: (err) => {},
+    // });
+  }
 }
