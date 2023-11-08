2bb58fab3997ada80d17d46423f5c8f6ab821d3f
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-08 19:46:03 +0300

    Update button.component.ts

--- src/app/Components/button/button.component.ts
+++ src/app/Components/button/button.component.ts
@@ -1,16 +1,30 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { ViewCell } from "ng2-smart-table";
 @Component({
   selector: "ngx-button",
   templateUrl: "./button.component.html",
   styleUrls: ["./button.component.scss"],
 })
-export class ButtonComponent implements ViewCell {
+export class ButtonComponent implements ViewCell, OnInit {
   @Input() value: string | number;
   @Input() rowData: any;
+  @Input() customValue: any;
+  @Output() saves: EventEmitter<any> = new EventEmitter();
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log("on in it");
+    console.log(this.customValue);
+  }
+
+  onClick() {
+    // this.saves.emit(this.rowData);
+    console.log(this.rowData)
+  }
 
   customAction() {
     // Define the custom action logic here
-    console.log('Custom action clicked for row:', this.rowData);
+    // console.log("Custom action clicked for row:", this.rowData, this.value);
+    console.log(this.customValue);
   }
 }
