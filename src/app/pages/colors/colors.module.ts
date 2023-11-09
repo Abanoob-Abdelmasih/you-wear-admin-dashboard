2c70e675defd0bac9ab517b4dc1a82032d3c3c43
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-09 17:19:57 +0300

    Update colors.module.ts

--- src/app/pages/colors/colors.module.ts
+++ src/app/pages/colors/colors.module.ts
@@ -4,9 +4,16 @@ import { CommonModule } from "@angular/common";
 import { ColorsRoutingModule } from "./colors-routing.module";
 import { AllColorsComponent } from "./all-colors/all-colors.component";
 import { ColorFormComponent } from "./color-form/color-form.component";
+import { NbCardModule } from "@nebular/theme";
+import { Ng2SmartTableModule } from "ng2-smart-table";
 
 @NgModule({
   declarations: [AllColorsComponent, ColorFormComponent],
-  imports: [CommonModule, ColorsRoutingModule],
+  imports: [
+    CommonModule,
+    ColorsRoutingModule,
+    NbCardModule,
+    Ng2SmartTableModule,
+  ],
 })
 export class ColorsModule {}
