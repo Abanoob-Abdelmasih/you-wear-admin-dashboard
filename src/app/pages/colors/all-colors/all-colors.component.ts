76ba09d2f458b7b08eb66c56de8d210a472e2c46
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-09 17:10:42 +0300

    Create all-colors.component.ts

---
+++ src/app/pages/colors/all-colors/all-colors.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-all-colors',
+  templateUrl: './all-colors.component.html',
+  styleUrls: ['./all-colors.component.scss']
+})
+export class AllColorsComponent {
+
+}
