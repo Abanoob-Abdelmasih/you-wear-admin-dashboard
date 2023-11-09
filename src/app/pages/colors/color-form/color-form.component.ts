b4b68a348e145425d471b01a0a5803580eff45cf
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-09 19:45:11 +0300

    Update color-form.component.ts

--- src/app/pages/colors/color-form/color-form.component.ts
+++ src/app/pages/colors/color-form/color-form.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'ngx-color-form',
-  templateUrl: './color-form.component.html',
-  styleUrls: ['./color-form.component.scss']
+  selector: "ngx-color-form",
+  templateUrl: "./color-form.component.html",
+  styleUrls: ["./color-form.component.scss"],
 })
 export class ColorFormComponent {
+  color: any;
 
+  showColor() {
+    console.log(this.color);
+  }
 }
