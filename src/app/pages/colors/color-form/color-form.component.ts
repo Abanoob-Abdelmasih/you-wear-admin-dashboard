96d85f2cef8474f722f8101d968b2e1ce02d82ed
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-09 17:10:45 +0300

    Create color-form.component.ts

---
+++ src/app/pages/colors/color-form/color-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-color-form',
+  templateUrl: './color-form.component.html',
+  styleUrls: ['./color-form.component.scss']
+})
+export class ColorFormComponent {
+
+}
