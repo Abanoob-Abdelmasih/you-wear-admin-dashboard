9daef7d5ce0218dda164fd710b5bf5de80db0d01
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-09 20:37:53 +0300

    Update colors.module.ts

--- src/app/pages/colors/colors.module.ts
+++ src/app/pages/colors/colors.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
+import {
+  NbActionsModule,
+  NbButtonModule,
+  NbCardModule,
+  NbCheckboxModule,
+  NbDatepickerModule,
+  NbIconModule,
+  NbInputModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbToggleModule,
+  NbUserModule,
+} from "@nebular/theme";
 import { ColorsRoutingModule } from "./colors-routing.module";
 import { AllColorsComponent } from "./all-colors/all-colors.component";
 import { ColorFormComponent } from "./color-form/color-form.component";
-import { NbCardModule } from "@nebular/theme";
 import { Ng2SmartTableModule } from "ng2-smart-table";
 import { ColorPickerModule } from "ngx-color-picker";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ThemeModule } from "../../@theme/theme.module";
 
 @NgModule({
   declarations: [AllColorsComponent, ColorFormComponent],
@@ -15,7 +28,20 @@ import { ColorPickerModule } from "ngx-color-picker";
     ColorsRoutingModule,
     NbCardModule,
     Ng2SmartTableModule,
-    ColorPickerModule
+    ColorPickerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NbToggleModule,
+    ThemeModule,
+    NbInputModule,
+    NbButtonModule,
+    NbActionsModule,
+    NbUserModule,
+    NbCheckboxModule,
+    NbRadioModule,
+    NbDatepickerModule,
+    NbSelectModule,
+    NbIconModule,
   ],
 })
 export class ColorsModule {}
