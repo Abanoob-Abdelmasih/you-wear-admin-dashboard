b4eed6d55d5e7c6d85c5963a2607f9bcb5fa5e66
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-09 23:01:04 +0300

    Update button.component.ts

--- src/app/Components/button/button.component.ts
+++ src/app/Components/button/button.component.ts
@@ -1,13 +1,17 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { ViewCell } from "ng2-smart-table";
 import { SizeService } from "../../Services/Size/size.service";
+import { ColorService } from "../../Services/Color/color.service";
 @Component({
   selector: "ngx-button",
   templateUrl: "./button.component.html",
   styleUrls: ["./button.component.scss"],
 })
 export class ButtonComponent implements ViewCell {
-  constructor(private sizeService: SizeService) {}
+  constructor(
+    private sizeService: SizeService,
+    private colorService: ColorService
+  ) {}
   @Input() value: string | number;
   @Input() rowData: any;
   @Input() type: any;
@@ -15,16 +19,30 @@ export class ButtonComponent implements ViewCell {
   @Output() saves: EventEmitter<any> = new EventEmitter();
 
   deleteRow(id: number) {
-    this.sizeService.deleteSize(id).subscribe({
-      next: (response) => {
-        if (response.status === 200) {
-          if (response.body.status === 200) {
-            // this.router.navigate(["/sizes"]);
-            this.sizeService.setSizes(response.body.data.sizes)
+    if (this.type === "colors") {
+      this.colorService.deleteColor(id).subscribe({
+        next: (response) => {
+          if (response.status === 200) {
+            if (response.body.status === 200) {
+              // this.router.navigate(["/sizes"]);
+              this.colorService.setColors(response.body.data.colors);
+            }
           }
-        }
-      },
-      error: (err) => {},
-    });
+        },
+        error: (err) => {},
+      });
+    } else {
+      this.sizeService.deleteSize(id).subscribe({
+        next: (response) => {
+          if (response.status === 200) {
+            if (response.body.status === 200) {
+              // this.router.navigate(["/sizes"]);
+              this.sizeService.setSizes(response.body.data.sizes);
+            }
+          }
+        },
+        error: (err) => {},
+      });
+    }
   }
 }
