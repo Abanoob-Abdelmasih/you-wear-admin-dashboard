d58bd4f51e77396eece876d6517a07dfc5fa3573
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-09 12:50:02 +0300

    Create active-badge.module.ts

---
+++ src/app/Components/active-badge/active-badge.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ActiveBadgeComponent } from "./active-badge.component";
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [],
+})
+export class ActiveBadgeModule {}
