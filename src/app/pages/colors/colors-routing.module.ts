55724e6c626ccff458421065d81be33d91779820
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-09 17:12:46 +0300

    Update colors-routing.module.ts

--- src/app/pages/colors/colors-routing.module.ts
+++ src/app/pages/colors/colors-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ColorFormComponent } from './color-form/color-form.component';
+import { AllColorsComponent } from './all-colors/all-colors.component';
+import { NotFoundComponent } from '../miscellaneous/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    // component: AllSizesComponent,
+    children: [
+      {
+        path: "",
+        component: AllColorsComponent,
+      },
+      {
+        path: "form",
+        component: ColorFormComponent,
+      },
+      {
+        path: ":id/edit",
+        component: ColorFormComponent,
+      },
+      {
+        path: "",
+        redirectTo: "",
+        pathMatch: "full",
+      },
+      {
+        path: "**",
+        component: NotFoundComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
