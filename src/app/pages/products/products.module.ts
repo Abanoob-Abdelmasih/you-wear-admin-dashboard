a5457500ea612a98d4c9af65797a693394c64a7c
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-10 23:26:47 +0300

    Update products.module.ts

--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from "@angular/common";
 
 import { ProductsRoutingModule } from "./products-routing.module";
 import { ProductFormComponent } from './product-form/product-form.component';
-
+import { NgxDropzoneModule } from 'ngx-dropzone';
 @NgModule({
   declarations: [
     ProductFormComponent
   ],
-  imports: [CommonModule, ProductsRoutingModule],
+  imports: [CommonModule, ProductsRoutingModule, NgxDropzoneModule],
 })
 export class ProductsModule {}
