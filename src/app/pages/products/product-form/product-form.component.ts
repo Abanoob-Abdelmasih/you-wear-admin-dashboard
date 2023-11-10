cdc871d60a0ed1971eeced8e31d561775345f0cf
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-10 23:26:50 +0300

    Update product-form.component.ts

--- src/app/pages/products/product-form/product-form.component.ts
+++ src/app/pages/products/product-form/product-form.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'ngx-product-form',
-  templateUrl: './product-form.component.html',
-  styleUrls: ['./product-form.component.scss']
+  selector: "ngx-product-form",
+  templateUrl: "./product-form.component.html",
+  styleUrls: ["./product-form.component.scss"],
 })
 export class ProductFormComponent {
+  // in app.component.ts
+  files: File[] = [];
 
+  onSelect(event) {
+    console.log(event);
+    this.files.push(...event.addedFiles);
+  }
+
+  onRemove(event) {
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
+  }
 }
