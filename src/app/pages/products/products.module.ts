fa5c4c1c3d50c291ac850e29975c412ae10ab227
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-10 23:44:51 +0300

    Update products.module.ts

--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -1,13 +1,38 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
+import {
+  NbActionsModule,
+  NbButtonModule,
+  NbCardModule,
+  NbCheckboxModule,
+  NbDatepickerModule,
+  NbIconModule,
+  NbInputModule,
+  NbRadioModule,
+  NbSelectModule,
+  NbToggleModule,
+  NbUserModule,
+} from "@nebular/theme";
 import { ProductsRoutingModule } from "./products-routing.module";
-import { ProductFormComponent } from './product-form/product-form.component';
-import { NgxDropzoneModule } from 'ngx-dropzone';
+import { ProductFormComponent } from "./product-form/product-form.component";
+import { NgxDropzoneModule } from "ngx-dropzone";
 @NgModule({
-  declarations: [
-    ProductFormComponent
+  declarations: [ProductFormComponent],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    NgxDropzoneModule,
+    NbActionsModule,
+    NbButtonModule,
+    NbCardModule,
+    NbCheckboxModule,
+    NbDatepickerModule,
+    NbIconModule,
+    NbInputModule,
+    NbRadioModule,
+    NbSelectModule,
+    NbToggleModule,
+    NbUserModule,
   ],
-  imports: [CommonModule, ProductsRoutingModule, NgxDropzoneModule],
 })
 export class ProductsModule {}
