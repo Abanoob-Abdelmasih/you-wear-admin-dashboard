beeffe01cf1f7f49af5e040859ff4a553a887b03
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-10 18:04:46 +0300

    Update products-routing.module.ts

--- src/app/pages/products/products-routing.module.ts
+++ src/app/pages/products/products-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from '../miscellaneous/not-found/not-found.component';
+import { ProductFormComponent } from './product-form/product-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    // component: AllSizesComponent,
+    children: [
+      // {
+      //   path: "",
+      //   component: AllSizesComponent,
+      // },
+      {
+        path: "form",
+        component: ProductFormComponent,
+      },
+      {
+        path: ":id/edit",
+        component: ProductFormComponent,
+      },
+      {
+        path: "",
+        redirectTo: "",
+        pathMatch: "full",
+      },
+      {
+        path: "**",
+        component: NotFoundComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
