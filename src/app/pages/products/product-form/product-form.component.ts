7e033bf785ded629a05fa9b7ac027884e039b430
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-11 14:29:38 +0300

    Update product-form.component.ts

--- src/app/pages/products/product-form/product-form.component.ts
+++ src/app/pages/products/product-form/product-form.component.ts
@@ -1,14 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { ColorService } from "../../../Services/Color/color.service";
+import { SizeService } from "../../../Services/Size/size.service";
 
 @Component({
   selector: "ngx-product-form",
   templateUrl: "./product-form.component.html",
   styleUrls: ["./product-form.component.scss"],
 })
-export class ProductFormComponent {
+export class ProductFormComponent implements OnInit {
+  //
+  constructor(
+    private colorService: ColorService,
+    private sizeService: SizeService
+  ) {}
+
   // variables
   activate?: boolean | undefined = true;
+  allSizes = [];
+  allColors = [];
+
+  ngOnInit(): void {
+    this.colorService.getAllColors().subscribe({
+      next: (response) => {
+        if (response.status === 200) {
+          if (response.body.status === 200) {
+            this.allColors = response.body.data.all_colors;
+          }
+        }
+      },
+      error: (err) => {},
+    });
+
+  }
 
   productForm = new FormGroup({
     colorSelected: new FormControl(null, Validators.required),
