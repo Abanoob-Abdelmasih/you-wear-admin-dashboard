40a5b63aca5e164094854366e3c022a9a9c0030b
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-13 19:49:39 +0300

    Update product.service.ts

--- src/app/Services/Product/product.service.ts
+++ src/app/Services/Product/product.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { ConfigService } from "../../Config/config.service";
+import { BehaviorSubject } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProductService {
+  constructor(private configServices: ConfigService) {}
 
-  constructor() { }
+  tempImages(getParams?: any, postParams?: any) {
+    return this.configServices
+      .postNewRequest("/test-image", getParams, postParams)
+      .pipe(
+        map((response) => {
+          return response;
+        })
+      );
+  }
 }
