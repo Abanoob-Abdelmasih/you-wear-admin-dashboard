8ef9ac06fc1eb0c383e4f783d2746015f520a936
Abanoob Abdelmasih <110768776+Abanoob-Abdelmasih@users.noreply.github.com>
2023-11-16 20:08:50 +0300

    Update convert-file.service.ts

--- src/app/Services/Convert/convert-file.service.ts
+++ src/app/Services/Convert/convert-file.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConvertFileService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getImage() {
+    return this.http
+      .get("http://127.0.0.1:8000/storage/product_images/6553b67260d2a.png", {
+        responseType: "arraybuffer"
+      })
+      .pipe(
+        map(response => {
+          return new File([response], "myImage.png");
+        })
+      );
+  }
 }
